0abae41e7dc6078ffc172f4fe4c661c7db64a261
kabradshaw1 <itllbok82@gmail.com>
2022-11-27 15:55:22 -0500

    upated some models and comment-controller

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment _id
+    // set custom id to avoid confusion with parent comment's _id field
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
@@ -14,27 +14,47 @@ const ReplySchema = new Schema(
     writtenBy: {
       type: String
     },
-    replies: [ReplySchema],
     createdAt: {
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     }
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true 
+    }
   }
 );
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
   }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
