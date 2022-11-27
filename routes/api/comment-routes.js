8e3fe1aa2c9bd692df7fb7e8744d3055a9ce02cd
kabradshaw1 <itllbok82@gmail.com>
2022-11-27 16:24:39 -0500

    update to ctonrollers and api for comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
@@ -7,5 +12,8 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
 
 module.exports = router;
